e0ff9b2cd7148add34ded6b73c6d83ed3c672e7f
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-05 10:13:53 +0100

    print files paths

--- lk.c
+++ lk.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <unistd.h>
+
+
 
 int main(){
 
     DIR *folder;
     struct dirent *file;
+    char dirPath[1000];
 
-    folder = opendir(".");
-    //folder = opendir("/home/cyan/Downloads/");
+    getcwd(dirPath, sizeof(dirPath));
+
+    //folder = opendir(".");
+    folder = opendir("/home/cyan/Downloads/");
 
     while( (file = readdir(folder)) != NULL ){
         printf("File name: %s\n", file->d_name);
+        char path[1500];
+
+        printf("File path: %s/%s\n", dirPath, file->d_name);
     }
 
     closedir(folder);
